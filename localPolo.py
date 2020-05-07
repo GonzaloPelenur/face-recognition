c2dfff6050539c7dc1fd9fcb23890ea50b23bf6d
Joaquin Polonuer <jtpolonuer@gmail.com>
2020-05-07 18:44:06 -0300

    server and local

---
+++ localPolo.py
@@ -0,0 +1,53 @@
+import face_recognition
+import cv2
+import requests
+import json
+import time
+FONT_THINCKNESS = 2
+FRAME_THICKNESS = 3
+MODEL = "cnn"
+threshold = 3  # Num of encodings
+send_encodigns = []
+counter = 0
+url = ''
+send = True
+video = cv2.VideoCapture(0)
+
+while True:
+    rect, image = video.read()
+    if not rect:
+        break
+
+    locations = face_recognition.face_locations(image, model=MODEL)
+    encodings = face_recognition.face_encodings(image, locations)
+
+    for face_encoding, face_location in zip(encodings, locations):
+        if counter <= threshold:
+            send_encodigns.append(face_encoding.tolist())
+            counter += 1
+        else:
+            # send the encondings
+            a = time.time()
+            r = requests.post("http://127.0.0.1:5000/compare",
+                              data=json.dumps({"data": send_encodigns}))
+            b = time.time()
+
+            timeToDetect = b-a
+
+            print(timeToDetect)
+
+            # reset variables
+            send_encodigns = []
+            counter = 0
+        top_left = (face_location[3], face_location[0])
+        bottom_right = (face_location[1], face_location[2])
+        color = [0, 0, 0]
+        cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+        send = False
+
+    cv2.imshow('', image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+video.release()
---
+++ serverPolo.py
@@ -0,0 +1,65 @@
+import random
+import matplotlib.pyplot as plt
+import timeit
+import time
+import pickle
+import cv2
+import os
+import face_recognition
+from flask import Flask, request
+from flask_cors import CORS
+import json
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)
+
+
+KNOWN_FACES_DIR = 'known_faces'
+TOLERANCE = 0.6
+FONT_THINCKNESS = 2
+FRAME_THICKNESS = 3
+MODEL = "cnn"
+
+video = cv2.VideoCapture(0)
+print('loading known faces')
+
+known_faces = []
+known_names = []
+
+
+for name in os.listdir(KNOWN_FACES_DIR):
+    print(name)
+    for filename in os.listdir(f'{KNOWN_FACES_DIR}\{name}'):
+        print(filename)
+        encoding = pickle.load(
+            open(f'{KNOWN_FACES_DIR}\{name}\{filename}', 'rb'))
+        known_faces.append(encoding)
+        known_names.append(int(name))
+
+if len(known_names) > 0:
+    next_id = max(known_names) + 1
+else:
+    next_id = 0
+
+
+@app.route("/compare", methods=["POST"])
+def handle_request():
+    req = json.loads(request.data)
+
+    results = []
+    for i in req["data"]:
+        recog = face_recognition.compare_faces(
+            known_faces, np.array(i), TOLERANCE)
+        print(recog)
+        if True in recog:
+            match = str(known_names[recog.index(True)])
+            results.append(match)
+            print(f'Match found: {match}')
+
+    res = {"status": str(results)}
+
+    return json.dumps(res)
+
+
+app.run()
