0baa8c74588c309624ed02b8c5c933c9675605d7
Joaquin Polonuer <jtpolonuer@gmail.com>
2020-05-27 15:05:42 -0300

    tried with face_recognition

--- rekog_polo/PersonDetector.py
+++ rekog_polo/PersonDetector.py
@@ -6,7 +6,6 @@ import time
 def start(video=0, frameRate=15):
     frameNum = 0
     cap = cv2.VideoCapture(video)
-    cap.set(cv2.CAP_PROP_FPS, 10)
 
     while cap.isOpened():
 
@@ -22,32 +21,32 @@ def start(video=0, frameRate=15):
             for detections in info:
                 if (detections[0] is 'person') and detections[1] > 100000:
                     continue
-                    # print(detections[2])
-                    # cv2.rectangle(image, (detections[2][0], detections[2][1]), (
-                    #     detections[2][2], detections[2][3]), (0, 0, 0), 2)
+        #             print(detections[2])
+        #             cv2.rectangle(image, (detections[2][0], detections[2][1]), (
+        #                 detections[2][2], detections[2][3]), (0, 0, 0), 2)
 
-                    #cv2.imshow('frame', image)
-                    # if cv2.waitKey(1) & 0xFF == ord('q'):
-                    #     break
+        # cv2.imshow('frame', image)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
 
 
 if __name__ == "__main__":
-    frate = 15
-    a = time.time()
-    start("media/videocolo.mp4", frate)
-    print("Detection framerate =", frate, "Time:", time.time()-a)
-
-    frate = 5
-    a = time.time()
-    start("media/videocolo.mp4", frate)
-    print("Detection framerate =", frate, "Time:", time.time()-a)
-
-    frate = 3
-    a = time.time()
-    start("media/videocolo.mp4", frate)
-    print("Detection framerate =", frate, "Time:", time.time()-a)
-
     frate = 1
     a = time.time()
     start("media/videocolo.mp4", frate)
     print("Detection framerate =", frate, "Time:", time.time()-a)
+
+    # frate = 5
+    # a = time.time()
+    # start("media/videocolo.mp4", frate)
+    # print("Detection framerate =", frate, "Time:", time.time()-a)
+
+    # frate = 3
+    # a = time.time()
+    # start("media/videocolo.mp4", frate)
+    # print("Detection framerate =", frate, "Time:", time.time()-a)
+
+    # frate = 1
+    # a = time.time()
+    # start("media/videocolo.mp4", frate)
+    # print("Detection framerate =", frate, "Time:", time.time()-a)
---
+++ rekog_polo/myversion.py
@@ -0,0 +1,52 @@
+import face_recognition
+import os
+import cv2
+import pickle
+import time
+import timeit
+import matplotlib.pyplot as plt
+import random
+
+KNOWN_FACES_DIR = 'known_faces'
+TOLERANCE = 0.6
+FONT_THINCKNESS = 2
+FRAME_THICKNESS = 3
+MODEL = "cnn"
+
+
+print('loading known faces')
+
+
+print('processing unknown faces')
+
+
+def start():
+    video = cv2.VideoCapture("media/videocolo.mp4")
+    while video.isOpened():
+        try:
+            rect, image = video.read()
+            image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+            locations = face_recognition.face_locations(image, model=MODEL)
+
+            for face_location in locations:
+
+                match = None
+
+            #     top_left = (face_location[3]-30, face_location[0]-30)
+            #     bottom_right = (face_location[1]+30, face_location[2]+30)
+
+            #     cv2.rectangle(image, top_left, bottom_right,
+            #                   (255, 0, 0), FRAME_THICKNESS)
+
+            # cv2.imshow('', image)
+
+            # if cv2.waitKey(1) & 0xFF == ord('q'):
+            #     break
+        except:
+            break
+
+
+if __name__ == "__main__":
+    a = time.time()
+    start()
+    print("Detection time:", time.time()-a)
